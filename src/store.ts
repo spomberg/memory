bfd4fd4c311ca05bbd253589d18f8ac330662681
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-06 18:26:39 -0400

    Import gridSlice

--- src/store.ts
+++ src/store.ts
@@ -3,13 +3,15 @@ import stateReducer from "./features/state/stateSlice";
 import themeReducer from "./features/theme/themeSlice";
 import playersReducer from "./features/players/playersSlice";
 import gridSizeReducer from "./features/gridSize/gridSizeSlice";
+import gridReducer from "./features/grid/gridSlice";
 
 export const store = configureStore({
   reducer : {
     state: stateReducer,
     theme: themeReducer,
     players: playersReducer,
-    gridSize: gridSizeReducer
+    gridSize: gridSizeReducer,
+    grid: gridReducer
   }
 });
 
