4c870dfad86fd5fca0ebb43e82fe7d3f5134a539
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-06 18:52:30 -0400

    Import states and implement useEffect to randomize the create when component is mounted

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,7 +1,25 @@
 import './Grid.scss';
 import Topbar from '../Topbar/Topbar';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+import { setGrid } from '../../features/grid/gridSlice';
+import { useEffect } from 'react';
+import generateNumberGrid from '../../helpers/helpers';
 
 export default function Grid() {
+  const dispatch = useAppDispatch();
+  const gridSize = useAppSelector((state) => state.gridSize.value);
+  const players = useAppSelector((state) => state.players.value);
+  const theme = useAppSelector((state) => state.theme.value);
+  const grid = useAppSelector((state) => state.grid.value);
+
+  useEffect(() => {
+    switch (theme) {
+      case 'numbers':
+        dispatch(setGrid(generateNumberGrid(gridSize)));
+        break;
+    }
+  }, [])
+  
   return (
     <>
       <Topbar />
