43e186ebd24bd69ff63d7149b9fe06edaf86e71e
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-06 15:03:39 -0400

    Create gridSlice

---
+++ src/features/grid/gridSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { RootState } from "../../store";
+
+interface gridState { value: number };
+
+const initialState: gridState = { value: 0 }
+
+export const gridSlice = createSlice({
+  name: 'grid',
+  initialState,
+  reducers: {
+    setGrid: (state, actions) => { state.value = actions.payload }
+  },
+}); 
+
+export const { setGrid } = gridSlice.actions;
+
+export const selectGrid = (state: RootState) => state.state.value;
+
+export default gridSlice.reducer;
