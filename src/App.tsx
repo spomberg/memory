e71c8ab246608685630e3f69a7331aaa3c4cf7ea
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-06 15:33:07 -0400

    Import state and use it to display component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.scss';
+import { useAppSelector } from './hooks/hooks';
 import Start from './components/Start/Start';
 
 function App() {
+  const state = useAppSelector((state) => state.state.value);
+
   return (
     <div className="App">
-      <Start />
+      {state === 'startPage' && <Start />}
     </div>
   );
 }
