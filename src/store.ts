1af8661afba9d4e84debd5e75660cecafa5e5637
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-06 14:59:29 -0400

    Import playersReducer

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import stateReducer from "./features/state/stateSlice";
 import themeReducer from "./features/theme/themeSlice";
+import playersReducer from "./features/players/playersSlice";
 
 export const store = configureStore({
   reducer : {
     state: stateReducer,
-    theme: themeReducer
+    theme: themeReducer,
+    players: playersReducer
   }
 });
 
