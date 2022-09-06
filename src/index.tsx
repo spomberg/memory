b280f102162e500927d444ea89fe5e260d14c568
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-06 14:52:09 -0400

    Import redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
