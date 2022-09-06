37cb68c5e97a8eac6204fb2eda1a7aaadc9fec48
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-06 13:55:54 -0400

    Add start page html

--- src/components/Start/Start.tsx
+++ src/components/Start/Start.tsx
@@ -2,6 +2,34 @@ import './Start.scss';
 
 export default function Start() {
   return (
-    <div>Start</div>
+    <div className='start'>
+      <h1>memory</h1>
+      <div className='game-settings'>
+        <div>
+          <span>Select Theme</span>
+          <div className='buttons-line'>
+            <button>Numbers</button> 
+            <button>Icons</button>
+          </div>
+        </div>
+        <div>
+          <span>Number of Players</span>
+          <div className='buttons-line'>
+            <button>1</button> 
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+          </div>
+        </div>
+        <div>
+          <span>Grid Size</span>
+          <div className='buttons-line'>
+            <button>4x4</button> 
+            <button>6x6</button>
+          </div>
+        </div>
+        <button className='start-button'>Start Game</button>
+      </div>
+    </div>
   )
 }
