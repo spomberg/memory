72b03d8508f2d11b0a68a2caf34129813b21bb46
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-07 17:38:21 -0400

    Import matched state and make buttons disabled if their index is in the array

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -2,6 +2,7 @@ import './Grid.scss';
 import Topbar from '../Topbar/Topbar';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { setGrid } from '../../features/grid/gridSlice';
+import { addMatchedTiles } from '../../features/matched/matchedSlice';
 import { useEffect } from 'react';
 import { generateNumberGrid, generateIconGrid } from '../../helpers/helpers';
 import { ReactSVG } from 'react-svg';
@@ -9,9 +10,9 @@ import { ReactSVG } from 'react-svg';
 export default function Grid() {
   const dispatch = useAppDispatch();
   const gridSize = useAppSelector((state) => state.gridSize.value);
-  const players = useAppSelector((state) => state.players.value);
   const theme = useAppSelector((state) => state.theme.value);
   const grid = useAppSelector((state) => state.grid.value);
+  const matched = useAppSelector((state) => state.matched.value);
 
   useEffect(() => {
     switch (theme) {
@@ -21,7 +22,7 @@ export default function Grid() {
       case 'icons':
         dispatch(setGrid(generateIconGrid(gridSize)));
     }
-  }, [])
+  }, [dispatch, gridSize, theme]);
   
   return (
     <>
@@ -30,7 +31,10 @@ export default function Grid() {
         {grid.map((tile: any, index: number) => {
           return (
             <li key={index}>
-              <div className='tile'>
+              <div 
+                className='tile'
+                {...matched.includes(index) && {disabled: true}}
+                >
                 {theme === 'numbers' ? <span>{tile}</span> : <ReactSVG src={tile} />}
               </div>
             </li>
