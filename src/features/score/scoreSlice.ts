2fe646982e8374a88357318c6a30f74594df5e9b
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-08 17:01:18 -0400

    Create scoreSlice

---
+++ src/features/score/scoreSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { RootState } from "../../store";
+
+interface scoreState { value: Array<number> };
+
+const initialState: scoreState = { value: [] }
+
+export const scoreSlice = createSlice({
+  name: 'score',
+  initialState,
+  reducers: {
+    initiateScore: (state, actions) => { 
+      const score = [];
+
+      for (let index = 0; index < actions.payload; index++) {
+        score.push(0);
+      }
+
+      state.value = score;
+     },
+    resetScore: (state) => { state.value = [] },
+    incrementScore: (state, actions) => { state.value[actions.payload]++ }
+  },
+}); 
+
+export const { initiateScore, resetScore, incrementScore } = scoreSlice.actions;
+
+export const scoreMoves = (state: RootState) => state.score.value;
+
+export default scoreSlice.reducer;
