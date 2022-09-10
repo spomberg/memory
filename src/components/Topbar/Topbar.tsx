a91065027f59620bfbcf4484213108f0335775cd
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-10 09:16:38 -0400

    Create reset function to reset states when using New Game and Restart buttons

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,18 +1,38 @@
 import './Topbar.scss';
 import { useAppDispatch } from '../../hooks/hooks';
 import { abortGame } from '../../features/state/stateSlice';
+import { resetGrid } from '../../features/grid/gridSlice';
+import { resetPlayer } from '../../features/currentPlayer/currentPlayerSlice';
+import { resetMoves } from '../../features/moves/movesSlice';
+import { resetMatchedTiles } from '../../features/matched/matchedSlice';
 
 export default function Topbar() {
   const dispatch = useAppDispatch();
+
+  // Resets states
+  const reset = () => {
+    dispatch(resetPlayer());
+    dispatch(resetMoves());
+    dispatch(resetMatchedTiles());
+  }
 
   return (
     <div className='topbar'>
       <h1>memory</h1>
       <div>
-        <button>
+        <button
+          onClick={() => {
+            reset();
+          }}
+        >
           Restart
         </button>
-        <button onClick={() => dispatch(abortGame())}>
+        <button 
+          onClick={() => {
+            reset();
+            dispatch(abortGame());
+            dispatch(resetGrid());
+        }}>
           New Game
         </button>
       </div>
