56041f8da44b0bfe2f1f3673466e38b100e5c6b9
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-10 09:52:26 -0400

    Generate new grid when restart is clicked

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,13 +1,17 @@
 import './Topbar.scss';
-import { useAppDispatch } from '../../hooks/hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { abortGame } from '../../features/state/stateSlice';
 import { resetGrid } from '../../features/grid/gridSlice';
 import { resetPlayer } from '../../features/currentPlayer/currentPlayerSlice';
 import { resetMoves } from '../../features/moves/movesSlice';
 import { resetMatchedTiles } from '../../features/matched/matchedSlice';
+import { setGrid } from '../../features/grid/gridSlice';
+import { generateNumberGrid, generateIconGrid } from '../../helpers/helpers';
 
 export default function Topbar() {
   const dispatch = useAppDispatch();
+  const theme = useAppSelector((state) => state.theme.value);
+  const gridSize = useAppSelector((state) => state.gridSize.value);
 
   // Resets states
   const reset = () => {
@@ -23,6 +27,13 @@ export default function Topbar() {
         <button
           onClick={() => {
             reset();
+            switch (theme) {
+              case 'numbers':
+                dispatch(setGrid(generateNumberGrid(gridSize)));
+                break;
+              case 'icons':
+                dispatch(setGrid(generateIconGrid(gridSize)));
+            }
           }}
         >
           Restart
