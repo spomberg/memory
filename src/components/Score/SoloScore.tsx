b0f4ec4b2a092eef963b00644a4a21412ccfa6a9
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-10 21:32:11 -0400

    Import the time state and display it on the score box

--- src/components/Score/SoloScore.tsx
+++ src/components/Score/SoloScore.tsx
@@ -3,11 +3,15 @@ import { useAppSelector } from '../../hooks/hooks';
 
 export default function SoloScore() {
   const moves = useAppSelector((state) => state.moves.value);
+  const time = useAppSelector((state) => state.timer.value);
 
   return (
     <div className='score'>
       <div className='score-box'>
         <span>Time</span>
+        <div className='time'>
+        <span>{time}</span>
+        </div>
       </div>
       <div className='score-box'>
         <span>Moves</span>
