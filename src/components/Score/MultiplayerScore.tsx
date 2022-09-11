2a37188fe26f025c889420116f3ad60b413b6d2c
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-11 10:10:11 -0400

    Import states and create score boxes with each players score

--- src/components/Score/MultiplayerScore.tsx
+++ src/components/Score/MultiplayerScore.tsx
@@ -1,7 +1,29 @@
 import './Score.scss';
+import { useAppSelector } from '../../hooks/hooks';
+import { convertPlayersToArr } from '../../helpers/helpers';
 
 export default function MultiplayerScore() {
+  const players = useAppSelector((state) => state.players.value);
+  const playersArr = convertPlayersToArr(players);
+  const score = useAppSelector((state) => state.score.value);
+  const currentPlayer = useAppSelector((state) => state.currentPlayer.value);
+
   return (
-    <div>MultiplayerScore</div>
+    <div className='score'>
+      <ul>
+        {playersArr.map((index: number) => {
+          return (
+            <li key={index}>
+              {currentPlayer === index && <div className='triangle'></div>}
+              <div className='score-box'>
+                <span>Player {index + 1}</span>
+                <span>{score[index]}</span>
+              </div>
+              {currentPlayer === index && <span className='current-turn'>CURRENT TURN</span>}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
