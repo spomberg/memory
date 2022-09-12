d2edf61d34cc336a4f81abe3d344abe0422871fb
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-11 22:06:35 -0400

    Import bootstrap modal

--- src/components/GameOver/GameOverSolo.tsx
+++ src/components/GameOver/GameOverSolo.tsx
@@ -1,7 +1,28 @@
 import './GameOver.scss';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { useAppSelector } from '../../hooks/hooks';
 
 export default function GameOverSolo() {
+  const state = useAppSelector((state) => state.state.value);
+  const players = useAppSelector((state) => state.players.value);
+  
   return (
-    <div>GameOverSolo</div>
+    <>
+      <Modal show={state === 'resultsPage' && players === 1 && true }>
+        <Modal.Header>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => console.log('Restart')}>
+            Restart
+          </Button>
+          <Button variant="primary" onClick={() => console.log('Start New Game')}>
+            Start New Game
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
