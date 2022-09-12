b5c02aea970731dc952fe18e4d62e0fcfe6b7ab8
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-12 18:56:04 -0400

    Set up isTied helper function

---
+++ src/helpers/isTied.ts
@@ -0,0 +1,10 @@
+/**
+ * Receives the score and returns false if the game was not tied or the tied score if it was 
+ * @param score number[]
+ * @returns boolean or number
+ */
+ function isTied(score: number[]) {
+  return null;
+}
+
+export { isTied };
